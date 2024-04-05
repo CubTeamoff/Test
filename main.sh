f1fccedf2847a34a82fcb538dd5028fbb704c039
CubTeam <164719394+CubTeamoff@users.noreply.github.com>
2024-04-06 00:20:27 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# Проверяем наличие исходной папки
-if [ -d "/storage/emulated/0/Android/data/com.foxdebug.acodefree/files/plugins/" ]
+# Проверяем, была ли передана команда "start"
+if [ "$1" == "start" ]
 then
-    # Копируем файлы из исходной папки в целевую папку
-    cp -r /storage/emulated/0/Android/data/com.foxdebug.acodefree/files/plugins/* /storage/emulated/0/Download/pluginsAcode/
-    
-    echo "Файлы успешно скопированы."
+    # Проверяем наличие исходной папки
+    if [ -d "/storage/emulated/0/Android/data/com.foxdebug.acodefree/files/plugins/" ]
+    then
+        # Копируем файлы из исходной папки в целевую папку
+        cp -r /storage/emulated/0/Android/data/com.foxdebug.acodefree/files/plugins/* /storage/emulated/0/Download/pluginsAcode/
+        
+        echo "Файлы успешно скопированы."
+    else
+        echo "Исходная папка не найдена. Проверьте путь к папке."
+    fi
 else
-    echo "Исходная папка не найдена. Проверьте путь к папке."
+    echo "Для запуска копирования файлов используйте команду: $0 start"
 fi
